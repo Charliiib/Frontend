2e621a301c093a9ddf43e23b63c682589b9c2740
Charli <barros.rcarlos@gmail.com>
2025-05-11 13:36:07 -0300

    agrega login y registro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import HeaderComponent from './components/HeaderComponent';
+import LoginComponent from './components/LoginComponent';
+import RegisterComponent from './components/RegisterComponent';
 import BannerComponent from './components/BannerComponent';
 
 function App() {
   return (
     <div>
     <HeaderComponent />
+    <LoginComponent/>
+    <RegisterComponent/>
     <BannerComponent />
     </div>
   );
