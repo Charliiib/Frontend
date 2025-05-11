a6d1b94cc57fffeaabf9308eb71c9b97695201cf
Charli <barros.rcarlos@gmail.com>
2025-05-11 14:03:43 -0300

    agrega resultados

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import RegisterComponent from './components/RegisterComponent';
 import BannerComponent from './components/BannerComponent';
 import SearchComponent from './components/SearchComponent';
 import SidebarComponent from './components/SidebarComponent';
+import ResultsComponent from './components/ResultsComponent';
 
 export const Container = ({ children }) => {
   return <div className="container">{children}</div>;
 };
+
+export const Row = ({ children }) => {
+  return <div className="row mt-5">{children}</div>;
+};
+
 
 function App() {
   return (
@@ -19,7 +25,10 @@ function App() {
     <BannerComponent />
     <Container>
     <SearchComponent />
+    <Row>
     <SidebarComponent />
+    <ResultsComponent />
+    </Row>
     </Container>
     </div>
   );
